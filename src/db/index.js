817d0c29a51c6edf620af561365a2ffa07baacfa
Trevor Waddell <trevorwaddell@Trevors-MBP.attlocal.net>
2020-04-30 09:20:53 -0400

    fixed default connection string

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 import { MongoClient as mongo } from 'mongodb';
 
-const url = process.env.MONGO_CONNECTION_URI || 'mongodb://localhost27017';
+const url = process.env.MONGO_CONNECTION_URI || 'mongodb://localhost:27017';
 
 export default async () => {
   const conn = await mongo.connect(url, {
